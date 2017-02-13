24fb3ab0487a230f3ed026374c58971b9cf02b5b
Michael <misha.kotliar@gmail.com>
2017-02-13 13:07:48 -0500

    Added get_data.sh script do download testing data from remote repository

---
+++ tests/data/get_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'Get data from https://github.com/michael-kotliar/workflow_test_data/archive/master.zip'
+wget https://github.com/michael-kotliar/workflow_test_data/archive/master.zip
+echo 'Extract data'
+unzip master.zip
+echo 'Delete unused files'
+mv workflow_test_data-master/* .
+rm -rf workflow_test_data-master
