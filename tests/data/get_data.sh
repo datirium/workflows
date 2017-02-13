5de77b350e5f3e25d7446cfe514fec0453ef68dd
Michael <misha.kotliar@gmail.com>
2017-02-13 16:43:46 -0500

    Updated link to get input testing data

--- tests/data/get_data.sh
+++ tests/data/get_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'Get data from https://github.com/michael-kotliar/workflow_test_data/archive/master.zip'
-wget https://github.com/michael-kotliar/workflow_test_data/archive/master.zip
+echo 'Get data from https://github.com/michael-kotliar/workflows_test_data/archive/master.zip'
+wget https://github.com/michael-kotliar/workflows_test_data/archive/master.zip
 echo 'Extract data'
 unzip master.zip
 echo 'Delete unused files'
