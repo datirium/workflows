2c59a1619d3faca44e8f5aab0a964b27a33d220f
Michael Kotliar <misha.kotliar@gmail.com>
2017-02-26 13:22:50 -0500

    Updated get_data script (download data for testing)

--- tests/data/get_data.sh
+++ tests/data/get_data.sh
@@ -4,5 +4,6 @@ wget https://github.com/michael-kotliar/workflows_test_data/archive/master.zip
 echo 'Extract data'
 unzip master.zip
 echo 'Delete unused files'
-mv workflow_test_data-master/* .
-rm -rf workflow_test_data-master
+mv workflows_test_data-master/* .
+rm -rf workflows_test_data-master
+rm master.zip
