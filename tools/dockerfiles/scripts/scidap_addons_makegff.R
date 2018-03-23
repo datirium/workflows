04c27121cb591fccc6c0376c6e39d95906083664
Michael Kotliar <misha.kotliar@gmail.com>
2018-03-23 11:35:48 -0400

    Update scidap-addons to v0.0.2
    Use updated scidap_addons_makegff.R
    script to support both MACS.xls and
    Iaintersect.tsv outputs. Updated
    makegff and rose tools, super-enhancer
    workflow. Add tests

--- tools/dockerfiles/scripts/scidap_addons_makegff.R
+++ tools/dockerfiles/scripts/scidap_addons_makegff.R
@@ -8,19 +8,26 @@ GFF_FILENAME = as.character(args[2])
 CONTROL_FILENAME = as.character(args[3])
 
 base_islands <- read.table(BASE_FILENAME, sep="\t", header=TRUE)
-base_islands = base_islands[base_islands[ ,"chrom"] != "chrM", ]
+
+CHROM_COL_NAME = "chrom"
+if ( !(CHROM_COL_NAME %in% colnames(base_islands)) )
+{
+    CHROM_COL_NAME = "chr"
+}
+
+base_islands = base_islands[base_islands[ ,CHROM_COL_NAME] != "chrM", ]
 
 if(!is.na(CONTROL_FILENAME)){
     control_islands <- read.table(CONTROL_FILENAME, sep="\t", header=TRUE)
-    control_islands = control_islands[control_islands[ ,"chrom"] != "chrM", ]
+    control_islands = control_islands[control_islands[ ,CHROM_COL_NAME] != "chrM", ]
 
     extended = 100000
-    control_islands.granges = GRanges(seqnames=control_islands[, "chrom"], ranges = IRanges(start = control_islands[, "start"] - extended, end = control_islands[, "end"] + extended))
+    control_islands.granges = GRanges(seqnames=control_islands[, CHROM_COL_NAME], ranges = IRanges(start = control_islands[, "start"] - extended, end = control_islands[, "end"] + extended))
     control_islands.granges = reduce(control_islands.granges)
-    base_islands.granges = GRanges(seqnames=base_islands[, "chrom"], ranges = IRanges(start = base_islands[, "start"], end= base_islands[, "end"]))
+    base_islands.granges = GRanges(seqnames=base_islands[, CHROM_COL_NAME], ranges = IRanges(start = base_islands[, "start"], end= base_islands[, "end"]))
     mark = as.matrix(findOverlaps(base_islands.granges, control_islands.granges, ignore.strand = T))
     base_islands = base_islands[-mark[ ,1], ]
 }
 
-gff_islands = cbind.data.frame(base_islands[,"chrom"],apply(base_islands,1,function(x) gsub(" ","",paste(x, collapse="_",sep="")) ),"", base_islands[ ,"start"],base_islands[ ,"end"],"",".","",apply(base_islands,1,function(x) gsub(" ","",paste(x, collapse="_",sep=""))))
+gff_islands = cbind.data.frame(base_islands[,CHROM_COL_NAME],apply(base_islands,1,function(x) gsub(" ","",paste(x, collapse="_",sep="")) ),"", base_islands[ ,"start"],base_islands[ ,"end"],"",".","",apply(base_islands,1,function(x) gsub(" ","",paste(x, collapse="_",sep=""))))
 write.table(gff_islands, GFF_FILENAME, quote=FALSE, col.names=FALSE, row.names=FALSE, append=FALSE, sep="\t")
